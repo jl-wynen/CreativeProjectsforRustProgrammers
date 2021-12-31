5c6d23f44ed28f151f3ce97f353ffb85289e04af
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-12-31 14:34:54 +0100

    Add toml_static

--- 01_Storing_and_Retrieving_Data/src/toml.rs
+++ 01_Storing_and_Retrieving_Data/src/toml.rs
@@ -1,10 +1,15 @@
+use serde_derive::Deserialize;
 use std::path::PathBuf;
+
+fn config_path() -> PathBuf {
+    let mut config_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    config_path.push("data/config.toml");
+    config_path
+}
 
 fn toml_dynamic() {
     let config_const_values = {
-        let mut config_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        config_path.push("data/config.toml");
-        let config_text = std::fs::read_to_string(&config_path).unwrap();
+        let config_text = std::fs::read_to_string(config_path()).unwrap();
         config_text.parse::<toml::Value>().unwrap()
     };
 
@@ -22,6 +27,59 @@ fn toml_dynamic() {
     );
 }
 
+#[allow(unused)]
+#[derive(Deserialize)]
+struct Input {
+    xml_file: String,
+    json_file: String,
+}
+
+#[allow(unused)]
+#[derive(Deserialize)]
+struct Redis {
+    host: String,
+}
+
+#[allow(unused)]
+#[derive(Deserialize)]
+struct Sqlite {
+    db_file: String,
+}
+
+#[allow(unused)]
+#[derive(Deserialize)]
+struct Postgresql {
+    username: String,
+    password: String,
+    host: String,
+    port: String,
+    database: String,
+}
+
+#[allow(unused)]
+#[derive(Deserialize)]
+struct Config {
+    input: Input,
+    redis: Redis,
+    sqlite: Sqlite,
+    postgresql: Postgresql,
+}
+
+fn toml_static() {
+    let config_const_values: Config = {
+        let config_text = std::fs::read_to_string(config_path()).unwrap();
+        toml::from_str(&config_text).unwrap()
+    };
+
+    println!(
+        "[Postgresql].Database: {}",
+        config_const_values.postgresql.database
+    );
+}
+
 fn main() {
+    println!("------------------------------\n  DYNAMIC");
     toml_dynamic();
+    println!("------------------------------\n  STATIC");
+    toml_static();
 }
